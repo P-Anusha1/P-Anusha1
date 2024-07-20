bc448558245e7fbdf65fbf67e09db517c9c489f4
Anusha <panusha282@gmail.com>
2024-07-20 13:09:44 +0530

    Basics java

---
+++ programs/datatypes.java
@@ -0,0 +1,14 @@
+package programs;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=20;
+		float f=2.3f;
+		System.out.println(n);
+		System.out.println(f);
+
+	}
+
+}
